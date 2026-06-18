586164d01dd7647f27df91fb6d2678d971df26a8
Traffic Wolf <flywinlucky777@gmail.com>
2026-06-18 23:33:15 +0300

    wef

--- Assets/My Game/Script/FPSPlayer.cs
+++ Assets/My Game/Script/FPSPlayer.cs
@@ -1,18 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class FPSPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 9f;
+    public float jumpHeight = 1.2f;
+    public float gravity = -20f;
+
+    [Header("Look")]
+    public Transform cameraTransform;
+    public float mouseSensitivity = 2f;
+    public float maxLookAngle = 89f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private float verticalRotation = 0f;
+    private bool isGrounded;
+    private float currentSpeed;
+
+    void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
+        currentSpeed = walkSpeed;
+
+        if (cameraTransform == null && Camera.main != null)
+            cameraTransform = Camera.main.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleLook();
+        HandleMovement();
+        HandleJumpAndGravity();
+    }
+
+    void HandleLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -maxLookAngle, maxLookAngle);
+
+        if (cameraTransform != null)
+            cameraTransform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
+
+    void HandleMovement()
+    {
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0f)
+            velocity.y = -2f;
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 input = (transform.right * h + transform.forward * v).normalized;
+
+        if (Input.GetKey(KeyCode.LeftShift) && v > 0f)
+            currentSpeed = runSpeed;
+        else
+            currentSpeed = walkSpeed;
+
+        controller.Move(input * currentSpeed * Time.deltaTime);
+    }
+
+    void HandleJumpAndGravity()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
